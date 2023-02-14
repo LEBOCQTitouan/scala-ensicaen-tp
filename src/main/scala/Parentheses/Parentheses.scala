ce2c599a574b79ca4ea8db91a57b2676c71c9c2b
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2023-02-14 10:15:46 +0100

    TP parenthesis

--- src/main/scala/Parentheses/Parentheses.scala
+++ src/main/scala/Parentheses/Parentheses.scala
@@ -1,12 +1,30 @@
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 package object Parentheses {
 
   // Écrire une fonction récursive qui indique si une phrase dispose de parenthèses bien construite
-  def equilibre(phrase: String): Boolean = ???
+  def equilibre(phrase: String): Boolean = {
+    equilibreGenerique('(', ')')(phrase)
+  }
 
   // pareil, mais générique
-  def equilibreGenerique(co: Char, cf: Char)(phrase: String): Boolean = ???
+  def equilibreGenerique(co: Char, cf: Char)(phrase: String): Boolean = {
+    if (phrase.equals("")) // TODO insert initial check for empty str in generique array
+      return true
+    equilibreGeneriqueArray(co, cf)(phrase.toCharArray)
+  }
+
+  @tailrec
+  def equilibreGeneriqueArray(co: Char, cf: Char)(charArray: Array[Char], startCo: Char = co, didFindOpening : Boolean = false): Boolean = {
+    if (charArray.length == 0) {
+      didFindOpening && startCo == co
+    } else if (charArray.head == co) {
+      equilibreGeneriqueArray(cf, co)(charArray.tail, startCo, didFindOpening = true)
+    } else
+      equilibreGeneriqueArray(co, cf)(charArray.tail, startCo, didFindOpening)
+  }
 
   // utiliser la fonction générique pour définir la version xml avec < et > comme caractère ouvrant/fermant
-  lazy val equilibreXml: String => Boolean = ???
-
+  lazy val equilibreXml: String => Boolean = (xml: String) => equilibreGeneriqueArray('<','>')(xml.toCharArray) // TODO not ok xml
 }
